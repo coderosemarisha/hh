9b060135fc210667bb416e8c7fb3dea51686e56e
coderosemarisha <marinabox74@gmail.com>
2019-10-11 19:18:24 +0300

    Added parsing for more pages and writing data to the file

--- hh.py
+++ hh.py
@@ -1,39 +1,64 @@
 import requests
+import csv
 from bs4 import BeautifulSoup as bs
-import pprint
 
 headers = {'accept':'*/*', 'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36 OPR/63.0.3368.107'}
 
 
-base_url = 'https://hh.ru/search/vacancy?area=77&search_period=3&text=python&page=0'
+base_url = 'https://hh.ru/search/vacancy?area=1&search_period=3&text=python&page=0'
 
 
 def hh_parse(base_url, headers):
 	jobs = []
+	urls = []
+	urls.append(base_url)
 	session = requests.Session()
 	request = session.get(base_url, headers=headers)
 
 	if request.status_code == 200:
-		soup = bs(request.content, 'html.parser')
+		soup = bs(request.content, 'lxml')
+		try:
+			pagination = soup.find_all('a', attrs={'data-qa':'pager-page'})
+			count = int(pagination[-1].text)
+			for i  in range(count):
+				url = f'https://hh.ru/search/vacancy?area=1&search_period=3&text=python&page={i}'
+				if url not in urls:
+					urls.append(url)
+		except:
+			pass
+
+	for url in urls:
+		request = session.get(url, headers=headers)
+		soup = bs(request.content, 'lxml')
 		divs = soup.find_all('div', attrs={'data-qa':'vacancy-serp__vacancy'})
 		for div in divs:
-			title = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-title'}).text
-			href = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-title'})['href']
-			company = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-employer'}).text
-			text1 = div.find('div', attrs={'data-qa':'vacancy-serp__vacancy_snippet_responsibility'}).text
-			text2 = div.find('div', attrs={'data-qa':'vacancy-serp__vacancy_snippet_requirement'}).text
-			content = text1 + ' ' + text2
-			jobs.append({
-				'title':title,
-				'href':href,
-				'company':company,
-				'content':content
-				})
-
-
-			pprint.pprint(jobs)
-			print("Hello Marina")
+			try:
+				title = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-title'}).text
+				href = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-title'})['href']
+				company = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-employer'}).text
+				text1 = div.find('div', attrs={'data-qa':'vacancy-serp__vacancy_snippet_responsibility'}).text
+				text2 = div.find('div', attrs={'data-qa':'vacancy-serp__vacancy_snippet_requirement'}).text
+				content = text1 + ' ' + text2
+				jobs.append({
+					'title':title,
+					'href':href,
+					'company':company,
+					'content':content
+					})
+			except:
+				pass
+			print(len(jobs))
 	else:
-		print('ERROR')
+		print('ERROR or DONE. Status code =  ' + str(request.status_code))
+	return jobs
 
-hh_parse(base_url, headers)
+def files_writer(jobs):
+	with open('parsed_jobs.csv', 'w')as file:
+		a_pen = csv.writer(file)
+		a_pen.writerow(('Title vacancy', 'URL', 'Name of company', 'Content vacancy'))
+		for job in jobs:
+			a_pen.writerow((job['title'], job['href'], job['company'], job['content']))
+
+
+jobs = hh_parse(base_url, headers)
+files_writer(jobs)
